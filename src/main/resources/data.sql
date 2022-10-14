0493a5991e314c8b3f72e2f2cd952da97c751559
gacerioni <gacerioni@gmail.com>
2022-10-14 12:12:53 -0300

    First working version with Prometheus and Dockerfile

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_harness_module (id, description, image_path, short_name, url) VALUES (53245235, 'Continuous Integration', 'CI.png', 'CI', 'https://harness.io');
+INSERT INTO tb_harness_module (id, description, image_path, short_name, url) VALUES (15352155, 'Continuous Delivery', 'CD.png', 'CD', 'https://harness.io');
+INSERT INTO tb_harness_module (id, description, image_path, short_name, url) VALUES (53151252, 'Feature Flags', 'FF.png', 'FF', 'https://harness.io');
+INSERT INTO tb_harness_module (id, description, image_path, short_name, url) VALUES (22344345, 'Cloud Cost Management', 'CCM.png', 'CCM', 'https://harness.io');
+INSERT INTO tb_harness_module (id, description, image_path, short_name, url) VALUES (12425251, 'Site Reliability Management', 'SRM.png', 'SRM', 'https://harness.io');
+INSERT INTO tb_harness_module (id, description, image_path, short_name, url) VALUES (75686858, 'Security Testing Orchestration', 'STO.png', 'STO', 'https://harness.io');
+INSERT INTO tb_harness_module (id, description, image_path, short_name, url) VALUES (09760679, 'Chaos Engineering', 'chaos.png', 'CHAOS', 'https://harness.io');
